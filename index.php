e02972b8c1e894452d350b6f36eccdbe5ca8f08f
ajayvibrant <ajay.vibrantappz@gmail.com>
2014-08-25 15:03:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+$parse = new parseRestClient(array(
+    'appid' => 'TrutDQheaqNBZTImcqEduRWIZE3YStmnpTohk2aJ',
+    'restkey' => '8W8kDjVEEzNP125fqoT54ib4fUswMOgBZ1qv82uR'
+));
+
 echo "hello world";
-echo "i want to see the output";
 ?>
